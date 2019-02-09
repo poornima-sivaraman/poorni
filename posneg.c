053b13fa9a35afd20d588d15601aef2f0fcf7405
poornima-sivaraman <32266574+poornima-sivaraman@users.noreply.github.com>
2019-02-09 15:29:34 +0530

    Update posneg.c

--- posneg.c
+++ posneg.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main(void) {
-	int n;
-	scanf("%d",&n);
-	if(n>0)
-	{
-		printf("Positive");
-	}
-	else if(n<0)
+	int x;
+	scanf("%d",&x);
+	if(x<0)
 	{
 		printf("Negative");
+	}
+	else if(n>0)
+	{
+		printf("Positive");
 	}
 	else
 	{
